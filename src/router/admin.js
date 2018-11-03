8a0dc794d77900d0e76f33fdf74ac1f09f6f32d0
Zhang.Cong <1084823834@qq.com>
2018-11-03 20:09:10 +0800

    refresh static files

--- src/router/admin.js
+++ src/router/admin.js
@@ -1,9 +1,18 @@
-import AdminLogin from '@/components/admin/AdminLogin'
-import AdminLayout from '@/components/admin/AdminLayout'
-import ArticleList from '@/components/admin/pages/ArticleList'
-import ArticleInfo from '@/components/admin/pages/ArticleInfo'
-import ProjectList from '@/components/admin/pages/ProjectList'
-import TagList from '@/components/admin/pages/TagList'
+// import AdminLogin from '@/components/admin/AdminLogin'
+// import AdminLayout from '@/components/admin/AdminLayout'
+// import ArticleList from '@/components/admin/pages/ArticleList'
+// import ArticleInfo from '@/components/admin/pages/ArticleInfo'
+// import ProjectList from '@/components/admin/pages/ProjectList'
+// import TagList from '@/components/admin/pages/TagList'
+
+const AdminLogin = () => import('@/components/admin/AdminLogin')
+const AdminLayout = () => import('@/components/admin/AdminLayout')
+const ArticleList = () => import('@/components/admin/pages/ArticleList')
+const ArticleInfo = () => import('@/components/admin/pages/ArticleInfo')
+const ProjectList = () => import('@/components/admin/pages/ProjectList')
+const TagList = () => import('@/components/admin/pages/TagList')
+
+
 
 export default [
   {
--- src/router/front.js
+++ src/router/front.js
@@ -1,9 +1,16 @@
-import Layout from '@/components/front/Layout'
-import Article from '@/components/front/pages/Article'
-import ArticleContent from '@/components/front/pages/ArticleContent'
-import About from '@/components/front/pages/About'
-import Main from '@/components/front/Main'
-import Project from '@/components/front/pages/Project'
+// import Layout from '@/components/front/Layout'
+// import Article from '@/components/front/pages/Article'
+// import ArticleContent from '@/components/front/pages/ArticleContent'
+// import About from '@/components/front/pages/About'
+// import Main from '@/components/front/Main'
+// import Project from '@/components/front/pages/Project'
+
+const Layout = () => import('@/components/front/Layout')
+const Article = () => import('@/components/front/pages/Article')
+const ArticleContent = () => import('@/components/front/pages/ArticleContent')
+const About = () => import('@/components/front/pages/About')
+const Main = () => import('@/components/front/Main')
+const Project = () => import('@/components/front/pages/Project')
 
 export default [
   {
