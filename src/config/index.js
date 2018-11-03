6ee416d019fda0410e29beaed755c8ab08443122
Zhang.Cong <1084823834@qq.com>
2018-11-03 23:59:24 +0800

    将端口改为3001 https端口

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'http://47.244.114.73:3000/api/'
+const SERVER_URL = 'http://47.244.114.73:3001/api/'
 const ROUTE_LIST = ['Main','Article','Project','About','ArticleContent']
 
 
