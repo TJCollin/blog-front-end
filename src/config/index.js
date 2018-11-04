c555daa8e5c0111f0e348481c626da9fb1a03e55
Zhang.Cong <1084823834@qq.com>
2018-11-04 14:56:21 +0800

    add server cert

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
-const SERVER_URL = 'http://47.244.114.73:3001/api/'
+// const SERVER_URL = 'http://47.244.114.73:3001/api/'
+const SERVER_URL = 'http://localhost:3000/api/'
 const ROUTE_LIST = ['Main','Article','Project','About','ArticleContent']
 
 
