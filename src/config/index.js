4d0d7ca1212b766232023f5e890f8f970883cb45
Zhang.Cong <1084823834@qq.com>
2018-11-04 15:23:52 +0800

    修改默认后端接口

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 // const SERVER_URL = 'http://47.244.114.73:3001/api/'
-const SERVER_URL = 'http://localhost:3000/api/'
+const SERVER_URL = 'https://blog.api.collinjs.site/api/'
 const ROUTE_LIST = ['Main','Article','Project','About','ArticleContent']
 
 
