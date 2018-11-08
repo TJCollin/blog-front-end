c4fe72594234112199b56cf0b3ab2967cc6989c4
Zhang.Cong <1084823834@qq.com>
2018-11-09 00:59:58 +0800

    about页面兼容ie

--- src/components/front/Layout.vue
+++ src/components/front/Layout.vue
@@ -242,7 +242,6 @@
     flex-direction column
     align-items: center
     width: 100%;
-    height: 100%;
     position: absolute;
     left: 0;
     top: 0;
@@ -307,6 +306,9 @@
   .content
     flex 1
     width: 900px
+    overflow-y visible
+    -ms-overflow-y scroll
+
     padding-bottom 20px
     #canvas
       position: fixed;
