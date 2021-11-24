f877c5b843639a7cad39b6dccc8ff498846d8180
collin <tjzhangcong@outlook.com>
2021-11-24 20:46:18 +0800

    change config

--- config/index.js
+++ config/index.js
@@ -8,8 +8,7 @@ module.exports = {
   dev: {
     // Paths
     assetsSubDirectory: "static",
-    assetsPublicPath:
-      process.env.NODE_ENV === "production" ? "/blog-front-end/" : "/",
+    assetsPublicPath: "/",
     proxyTable: {},
 
     // Various Dev Server settings
@@ -42,7 +41,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, "../dist"),
     assetsSubDirectory: "static",
-    assetsPublicPath: "/",
+    assetsPublicPath:
+      process.env.NODE_ENV === "production" ? "/blog-front-end/" : "/",
 
     /**
      * Source Maps
