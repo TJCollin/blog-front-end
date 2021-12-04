8cbb0dc7532a395e27d9f82e58ea4926a2bf0ef2
collin <tjzhangcong@outlook.com>
2021-12-04 16:08:30 +0800

    change the public path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/blog-front-end/'
+    : '/',
   chainWebpack: (config) => {
     config.externals({
       BMap: "BMap",
