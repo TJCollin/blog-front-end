6df49424f6e12b3a3b01a7174602cba342b310ed
collin <tjzhangcong@outlook.com>
2021-12-04 16:16:36 +0800

    change serve url

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,5 @@
 const SERVER_NAME =
-  process.env.NODE_ENV === "production"
-    ? "https://blog.api.collinjs.site/"
-    : "https://next-test-beta-beryl.vercel.app/";
+  process.env.NODE_ENV === "https://next-test-beta-beryl.vercel.app/";
 const SERVER_URL = `${SERVER_NAME}api/`;
 const ROUTE_LIST = ["Main", "Article", "Project", "About", "ArticleContent"];
 
